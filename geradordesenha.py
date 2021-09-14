d21858469adc46cc49c6e2053a9c079edbd37691
Gilmarfilho2003 <89646761+Gilmarfilho2003@users.noreply.github.com>
2021-09-14 19:20:41 -0300

    Update geradordesenha.py

--- geradordesenha.py
+++ geradordesenha.py
@@ -1,1 +1,46 @@
 
+from random import seed, random
+
+seed(1)
+print(random()) #0.134364244112
+print(random()) #0.847433736937
+
+seed(1)
+print(random()) #0.134364244112
+print(random()) #0.847433736937
+
+from random import seed, random
+ print(random()) # 0.885350193339
+ print(random()) # 0.381136740351
+
+ print(random()) # 0.634755066626
+ print(random()) # 0.130661237179
+
+import string
+
+string.ascii_lowercase # abcdefghijklmnopqrstuvwxyz
+string.ascii_uppercase # ABCDEFGHIJKLMNOPQRSTUVWXYZ
+string.ascii_letters # abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+string.digits # 0123456789
+string.punctuation # <=>?@[\]^_`{|}~.
+
+from random import choice
+import string
+
+tamanho = 5
+valores = string.ascii_lowercase
+senha = ''
+for i in range(tamanho):
+  senha += choice(valores)
+
+print(senha)
+
+tamanho = 5
+valores = string.ascii_lowercase + string.digits
+senha = ''
+for i in range(tamanho):
+  senha += choice(valores)
+
+print(senha)
+
+valores = string.ascii_letters + string.digits + string.punctuation
